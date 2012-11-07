fd87928a94544bfb6d2ad86eb18081efa67b6e74
Carlos Aguilar de la Morena <neko_250@ymail.com>
2012-11-07 22:50:16 +0100

    New Game

---
+++ Last Stand Game/LastSt.cpp
@@ -0,0 +1,139 @@
+/*
+       ==========================================
+       ||                                      ||
+       ||            Source coded by:          ||
+       ||      Carlos Aguilar de la Morena     ||
+       ||                                      ||
+       ||           At  1 / 11 / 2012          ||
+       ||                                      ||
+       ==========================================
+*/
+
+#include <iostream>
+#include <stdlib.h>
+#include <ctime>
+#include <cstdlib>
+using namespace std;
+
+int main()
+{
+	int obj, turn, move;
+
+	//Seleccion de Turno
+	srand(time(NULL));
+	if((int(rand()) % 2) == 0)
+	{
+		turn = 1;
+	}
+	else
+	{
+		turn = 2;
+	}
+
+	//Bienvenida, reglas y preparacion.
+	cout << "\n\tBienvenido\n\n"
+		 << "==========================\n\n"
+		 << "Reglas del juego:\n"
+		 << "El jugador define un numero determinado de objetos.\n"
+		 << "La CPU decide quien empieza y, por turnos, la CPU y\n"
+		 << "el jugador van retirando objetos. El numero de objetos\n"
+		 << "que se pueden retirar en cada turno es de 1, 2 o 3.\n"
+		 << "El que retire el ultimo objeto, pierde.\n\n"
+		 << "==========================\n\n"
+		 << "Defina el numero de objetos en esta partida: ";
+	cin >> obj;
+	cout << "Empieza la partida ";
+
+	if(turn == 1)
+	{
+		cout << "el jugador.\n";
+	}
+	else
+	{
+		cout << "la CPU.\n";
+	}
+
+	//Comienza el juego
+	while(obj > 0)
+	{
+		cout << "\n==========================\n\n"
+			 << "Objetos restantes: " << obj << endl;
+
+		//Turno jugador
+		if(turn == 1)
+		{
+			cout << "Cuantos objetos desea retirar? ";
+			cin >> move;
+
+			//Error de movimiento
+			while((move < 1) || (3 < move))
+			{
+				cout << "Solo se pueden retirar 1, 2 o 3 objetos por turno.\n"
+					 << "Cuantos objetos desea retirar? ";
+				cin >> move;
+			}
+
+			obj -= move;
+			turn = 2;
+		}
+		//Turno CPU
+		else
+		{
+			//Seleccion de movimiento
+			if(obj >= 3)
+			{
+				if((int(rand()) % 3) == 0)
+				{
+					cout << "La maquina retira 1 objeto.\n";
+					obj -= 1;
+				}
+				else if((int(rand()) % 3) == 1)
+				{
+					cout << "La maquina retira 2 objetos.\n";
+					obj -= 2;
+				}
+				else if((int(rand()) % 3) == 2)
+				{
+					cout << "La maquina retira 3 objetos.\n";
+					obj -= 3;
+				}
+			}
+			else if(obj == 2)
+			{
+				if((int(rand()) % 2) == 0)
+				{
+					cout << "La maquina retira 1 objeto.\n";
+					obj -= 1;
+				}
+				else if((int(rand()) % 2) == 1)
+				{
+					cout << "La maquina retira 2 objetos.\n";
+					obj -= 2;
+				}
+			}
+			else
+			{
+				cout << "La maquina retira 1 objeto.\n";
+				obj -= 1;
+			}
+
+			turn = 1;
+		}
+	}
+
+	cout << "\n==========================\n\n";
+
+	if(turn == 1)
+	{
+		cout << "\nEnhorabuena! Has ganado\n\n"
+			 << "==========================\n\n";
+	}
+	else
+	{
+		cout << "\nGame Over, gana la CPU\n\n"
+			 << "==========================\n\n";
+	}
+
+	system("pause");
+	return 0;
+}
