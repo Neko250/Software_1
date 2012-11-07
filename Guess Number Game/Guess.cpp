4e428760011351188cddbf2d50d2769ed09804dc
Carlos Aguilar de la Morena <neko_250@ymail.com>
2012-11-07 22:50:06 +0100

    Fixed a bug with limits

--- Guess Number Game/Guess.cpp
+++ Guess Number Game/Guess.cpp
@@ -4,7 +4,7 @@
        ||            Source coded by:          ||
        ||      Carlos Aguilar de la Morena     ||
        ||                                      ||
-       ||           At  30 / 10 / 2012         ||
+       ||           At  1 / 11 / 2012          ||
        ||                                      ||
        ==========================================
 */
@@ -17,35 +17,40 @@ using namespace std;
 
 int main()
 {
-    int lim_inf, lim_sup, var;
+    int lim_inf, lim_sup, range, var;
     char user('a');
 
+    //Determinacion de limites
     cout << "Piense un numero en un rango determinado.\n"
     	 << "Introduzca el limite inferior: ";
     cin >> lim_inf;
     cout << "Introduzca el limite superior: ";
     cin >> lim_sup;
 
+    //Calculo semilla rand()
     srand(time(NULL));
 
+    //Comienza el juego
     while(user != '=')
     {
-    	do
-    	{
-    		var = ((rand()) % (lim_sup));
-    	}
-    	while(var <= lim_inf);
+		//Calculo longitud del rango (Limites incluidos: Diferencia +1)
+		range = lim_sup - lim_inf + 1;
+		
+    	//Variable dentro del rango
+    	var = ((int(rand()) % range) + lim_inf);
 
     	cout << "Es este su numero? " << var << endl
     		 << "Introduzca '<', '>' o '=', segun aplique: ";
     	cin >> user;
 
+    	//Input y asignacion de limites nuevos
     	if(user == '<')
-    		lim_sup = var;
+    		lim_sup = var - 1;
     	if(user == '>')
-    		lim_inf = var;
+    		lim_inf = var + 1;
     }
 
+    //Fin
     cout << "\nGame Over\n\n";
 
     system("pause");
