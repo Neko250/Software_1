112d2580a43b06531e89999dd9dbf4710d9d8a08
Carlos Aguilar de la Morena <neko_250@ymail.com>
2013-03-04 23:49:52 +0100

    New Res

---
+++ Resources/neko250.cpp
@@ -0,0 +1,337 @@
+
+//=======================================================
+//
+//	Da un numero aleatorio entre [0, num - 1]
+
+int random(int num)
+{
+	srand(time(NULL));
+	
+	return (rand() % num);
+}
+
+//=======================================================
+//
+//	Potencia
+
+double pow(double a, int b)
+{
+	double ans(1);
+
+	for(int i = 1; i <= b; i++)
+	{
+		ans *= a;
+	}
+
+	return ans;
+}
+
+//==========================================================
+//
+//	Factorial
+
+double fact(double X)
+{
+	double ans = 1;
+	
+	for(int i = 1; i <= X; i++)
+	{
+		ans *= i;
+	}
+	
+	return ans;
+}
+
+//=======================================================
+//
+//	Numero de digitos de 'N'
+
+unsigned digits(unsigned N)
+{
+	unsigned digits(1);
+
+	for(unsigned mod = 10; (N / mod) != 0; mod *= 10)
+	{
+		digits++;
+	}
+
+	return digits;
+}
+
+//=======================================================
+//
+//	Calcula el digito en la posicion i-esima de 'N'
+
+unsigned pos_calc(unsigned N, unsigned i)
+{
+	unsigned position;
+
+	position = (N / int(pow(double(10), (i - 1)))) % 10;
+	return position;
+}
+
+//=======================================================
+//
+//	Muestra si un numero es primo o no
+
+bool prime(unsigned N)
+{
+	bool prime = true;
+	
+	for(unsigned divisor = 2; divisor < N; divisor++)
+	{
+		if(N % divisor == 0)
+		{
+			prime = false;
+		}
+	}
+
+	return prime;
+}
+
+//=======================================================
+//
+//	Raiz cuadrada
+
+double sqrt(double x)
+{
+	double r = x, t = 0;
+	
+	while(t != r)
+	{
+		t = r;
+		r = (((x / r) + r) / 2);
+	}
+	
+	return r;
+}
+
+//=======================================================
+//
+//	Dibuja un triangulo de base y altura 'n'
+
+void triangle(unsigned n)
+{
+	for(unsigned fila(1); fila <= n; fila++)
+	{
+		cout << "\t";
+		spaces(fila, n);
+		draw(fila);
+		cout << endl;
+	}
+}
+
+//-------------------------------------------------------
+
+void spaces(unsigned fila, unsigned n)
+{
+	for(unsigned i(1); i <= (n - fila); i++)
+	{
+		cout << " ";
+	}
+}
+
+//-------------------------------------------------------
+
+void draw(unsigned fila)
+{
+	for(unsigned i(1); i <= ((2 * fila) - 1); i++)
+	{
+		if(i % 2 == 1)
+		{
+			cout << "*";
+		}
+		else
+		{
+			cout << " ";
+		}
+	}
+}
+
+//=======================================================
+//
+//	Guardar texto en un archivo
+
+void save_text()
+{
+	ofstream output;
+	output.open("file.txt");
+	char text(' ');
+
+	system("cls");
+	cout << endl;
+	cout << "Introduzca un texto, acabado en una barra espaciadora\n"
+		 << "invertida para indicar el final del texto, y pulse enter:\n\n";
+
+	while(text != '\\')
+	{
+		cin.get(text);
+		output << text;
+	}
+
+	system("cls");
+	cout << endl;
+	cout << "\nSu archivo se ha guardado exitosamente.";
+	Sleep(1500);
+}
+
+//=======================================================
+//
+//	Mostrar texto de un archivo
+
+void load_text()
+{
+	ifstream input;
+	input.open("file.txt");
+	char text(' ');
+
+	system("cls");
+	cout << endl;
+	cout << "Texto guardado en 'file.txt':\n\n";
+
+	while(text != '\\')
+	{
+		input.get(text);
+		cout << text;
+	}
+
+	cout << "\n\n";
+	system("pause");
+}
+
+//========================================================
+//
+//	Inicializar Pantalla
+
+void set_scrn()
+{
+	system("cls");
+	cout << "\n\t";
+}
+
+//========================================================
+//
+//	WELCOME !!!
+
+void welcome()
+{
+	system("cls");
+	cout << endl << endl
+		 << "\t _ _ _\n"
+		 << "\t| | | |\n"
+		 << "\t| | | |\n"
+		 << "\t|_____|";
+	Sleep(500);
+	system("cls");
+	cout << endl << endl
+		 << "\t _ _ _\n"
+		 << "\t| | | |___\n"
+		 << "\t| | | | -_|\n"
+		 << "\t|_____|___|";
+	Sleep(500);
+	system("cls");
+	cout << endl << endl
+		 << "\t _ _ _     _\n"
+		 << "\t| | | |___| |\n"
+		 << "\t| | | | -_| |\n"
+		 << "\t|_____|___|_|";
+	Sleep(500);
+	system("cls");
+	cout << endl << endl
+		 << "\t _ _ _     _\n"
+		 << "\t| | | |___| |___\n"
+		 << "\t| | | | -_| |  _|\n"
+		 << "\t|_____|___|_|___|";
+	Sleep(500);
+	system("cls");
+	cout << endl << endl
+		 << "\t _ _ _     _\n"
+		 << "\t| | | |___| |___ ___\n"
+		 << "\t| | | | -_| |  _| . |\n"
+		 << "\t|_____|___|_|___|___|";
+	Sleep(500);
+	system("cls");
+	cout << endl << endl
+		 << "\t _ _ _     _\n"
+		 << "\t| | | |___| |___ ___ _____\n"
+		 << "\t| | | | -_| |  _| . |     |\n"
+		 << "\t|_____|___|_|___|___|_|_|_|";
+	Sleep(500);
+	system("cls");
+	cout << endl << endl
+		 << "\t _ _ _     _\n"
+		 << "\t| | | |___| |___ ___ _____ ___\n"
+		 << "\t| | | | -_| |  _| . |     | -_|\n"
+		 << "\t|_____|___|_|___|___|_|_|_|___|";
+	Sleep(2000);
+}
+
+//========================================================
+//
+//	Limpia el bufer de entrada
+
+void clearBufer()
+{
+	HANDLE hConsole = GetStdHandle(STD_INPUT_HANDLE);
+	FlushConsoleInputBuffer(hConsole);
+}
+
+//========================================================
+//
+//	Situa el cursor de la consola en (x, y)
+
+void setCursor(int x, int y)
+{
+	COORD pos;
+	pos.X = x;
+	pos.Y = y;
+	
+	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), pos);
+}
+
+//========================================================
+//
+//	Situa el cursor de la consola en 'pos'
+
+void setCursor(COORD pos)
+{
+	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), pos);
+}
+
+//========================================================
+//
+//	Añade color al siguiente texto impreso
+
+void setColor(int color)
+{
+	SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), color);
+}
+
+//========================================================
+//
+//	Esconde el cursor de la consola
+
+void hideConsoleCursor()
+{
+	CONSOLE_CURSOR_INFO ConCurInf;
+	ConCurInf.dwSize = 100;
+	ConCurInf.bVisible = false;
+	SetConsoleCursorInfo(GetStdHandle(STD_OUTPUT_HANDLE), &ConCurInf);
+}
+
+//========================================================
+//
+//	Muestra el cursor de la consola
+
+void showConsoleCursor()
+{
+	CONSOLE_CURSOR_INFO ConCurInf;
+	ConCurInf.dwSize = 100;
+	ConCurInf.bVisible = true;
+	SetConsoleCursorInfo(GetStdHandle(STD_OUTPUT_HANDLE), &ConCurInf);
+}
+
+//========================================================
+//
+//	
+
---
+++ Resources/neko250.h
@@ -0,0 +1,226 @@
+
+// =======================================================
+// ||    ===========================================    ||
+// ||  =====                                     =====  ||
+// || ====              Library Coded              ==== ||
+// ||====                    by                     ====||
+// || ====                 Neko250                 ==== ||
+// ||  =====                                     =====  ||
+// ||    ===========================================    ||
+// =======================================================
+
+//=====================================================
+//
+//	Cabeceras
+
+/*
+                                                               .---.
+                                                              /  .  \
+                                                             |\_/|   |
+                                                             |   |  /|
+  .----------------------------------------------------------------' |
+ /  .-.                                                              |
+|  /   \       Programa creado por Carlos Aguilar de la Morena       |
+| |\_.  |                                                            |
+|\|  | /|                          El                                |
+| `---' |                                                            |
+|       |                    dd / mm / yyyy                          |
+|       |                                                           /
+|       |----------------------------------------------------------'
+\       |
+ \     /
+  `---'
+
+*/
+
+/*
+       ==========================================
+       ||                                      ||
+       ||          Programa creado por:        ||
+       ||      Carlos Aguilar de la Morena     ||
+       ||                                      ||
+       ||           El dd / mm / yyyy          ||
+       ||                                      ||
+       ==========================================
+*/
+
+//=====================================================
+//
+//	Includes y Prototipos
+
+#include <iostream>
+#include <stdlib.h>
+#include <windows.h>
+#include <fstream>
+#include <time.h>
+#include <string.h>
+using namespace std;
+
+int random(int);
+double pow(double, int);
+double fact(double);
+unsigned digits(unsigned);
+unsigned pos_calc(unsigned, unsigned);
+bool prime(unsigned);
+double sqrt(double);
+
+void triangle(unsigned);
+void spaces(unsigned, unsigned);
+void draw(unsigned);
+
+void save_text();
+void load_text();
+
+void set_scrn();
+void welcome();
+
+void clearBufer();
+void setCursor(int, int);
+void setCursor(COORD);
+void setColor(int);
+void hideConsoleCursor();
+void showConsoleCursor();
+
+//=====================================================
+//
+//	Tipos y Constantes
+
+const char block = 219;
+const char beep = 7;
+
+enum color
+{
+	/* 0 */ black,
+	/* 1 */ blue,
+	/* 2 */ green,
+	/* 3 */ aqua,
+	/* 4 */ red,
+	/* 5 */ purple,
+	/* 6 */ gold,
+	/* 7 */ light_light_gray,
+	/* 8 */ light_gray,
+	/* 9 */ light_blue,
+	/* 10 */ light_green,
+	/* 11 */ light_aqua,
+	/* 12 */ light_red,
+	/* 13 */ pink,
+	/* 14 */ white,
+	/* 15 */ gray
+};
+
+//=====================================================
+//
+//	Definicion funciones
+
+#include "neko250.cpp"
+
+//=====================================================
+//
+//	Virtual Keys
+
+#define VK_Mouse1 1
+#define VK_Mouse2 2
+
+#define VK_Num0 96
+#define VK_Num1 97
+#define VK_Num2 98
+#define VK_Num3 99
+#define VK_Num4 100
+#define VK_Num5 101
+#define VK_Num6 102
+#define VK_Num7 103
+#define VK_Num8 104
+#define VK_Num9 105
+
+#define VK_NumProd 106
+#define VK_NumPlus 107
+#define VK_NumMinus 109
+#define VK_NumDiv 111
+#define VK_NumDec 110
+
+#define VK_0 48
+#define VK_1 49
+#define VK_2 50
+#define VK_3 51
+#define VK_4 52
+#define VK_5 53
+#define VK_6 54
+#define VK_7 55
+#define VK_8 56
+#define VK_9 57
+
+#define VK_A 65
+#define VK_B 66
+#define VK_C 67
+#define VK_D 68
+#define VK_E 69
+#define VK_F 70
+#define VK_G 71
+#define VK_H 72
+#define VK_I 73
+#define VK_J 74
+#define VK_K 75
+#define VK_L 76
+#define VK_M 77
+#define VK_N 78
+#define VK_O 79
+#define VK_P 80
+#define VK_Q 81
+#define VK_R 82
+#define VK_S 83
+#define VK_T 84
+#define VK_U 85
+#define VK_V 86
+#define VK_W 87
+#define VK_X 88
+#define VK_Y 89
+#define VK_Z 90
+
+#define VK_Egne 192
+#define VK_Cedilla 191
+#define VK_Plus 187
+
+// No funcionan correctamente en la consola de Windows
+//
+//#define VK_F1 112
+//#define VK_F2 113
+//#define VK_F3 114
+//#define VK_F4 115
+//#define VK_F5 116
+//#define VK_F6 117
+//#define VK_F7 118
+//#define VK_F8 119
+//#define VK_F9 120
+//#define VK_F11 122
+//#define VK_F12 123
+
+#define VK_LCtrl 162
+#define VK_RCtrl 163
+#define VK_LShift 160
+#define VK_RShift 161
+#define VK_Alt 164
+#define VK_AltGr 165
+#define VK_Space 32
+#define VK_BloqMayus 20
+#define VK_BloqNum 144
+#define VK_Windows 91
+#define VK_Tab 9
+#define VK_App 93
+#define VK_Return 13
+#define VK_Backspace 8
+#define VK_Supr 46
+#define VK_Esc 27
+#define VK_Insert 45
+#define VK_Start 36
+#define VK_End 35
+#define VK_PrevPag 33
+#define VK_NextPag 34
+
+#define VK_Up 38
+#define VK_Left 37
+#define VK_Down 40
+#define VK_Right 39
+
+#define VK_Dot 190
+#define VK_Comma 188
+#define VK_Line 189
